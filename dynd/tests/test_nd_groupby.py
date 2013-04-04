02f4211fa09e8bd9f4a54d296e1f9bc7dadd5452
Mark Wiebe <mwwiebe@gmail.com>
2013-04-04 01:19:20 -0700

    Add test of groupby with immutable 'by'

---
+++ dynd/tests/test_nd_groupby.py
@@ -0,0 +1,24 @@
+import sys
+import unittest
+from dynd import nd, ndt
+
+class TestGroupBy(unittest.TestCase):
+    def test_immutable(self):
+        a = nd.ndobject([
+                ('x', 0),
+                ('y', 1),
+                ('x', 2),
+                ('x', 3),
+                ('y', 4)],
+                udtype='{A: string; B: int32}').eval_immutable()
+        gb = nd.groupby(a, nd.fields(a, 'A'))
+        self.assertEqual(nd.as_py(gb.groups), [{'A': 'x'}, {'A': 'y'}])
+        self.assertEqual(nd.as_py(gb), [
+                [{'A': 'x', 'B': 0},
+                 {'A': 'x', 'B': 2},
+                 {'A': 'x', 'B': 3}],
+                [{'A': 'y', 'B': 1},
+                 {'A': 'y', 'B': 4}]])
+
+if __name__ == '__main__':
+    unittest.main()

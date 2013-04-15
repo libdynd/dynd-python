6c8abb3fe830769195f8b84faac369d0d8b125c6
Mark Wiebe <mwwiebe@gmail.com>
2013-04-15 12:17:54 -0700

    Add documentation for make_computed_fields

--- dynd/nd/computed_fields.py
+++ dynd/nd/computed_fields.py
@@ -126,6 +126,58 @@ def add_computed_fields(n, fields, rm_fields=[], fnname=None):
     return elwise_map([n], fieldexpr, result_udt)
 
 def make_computed_fields(n, replace_undim, fields, fnname=None):
+    """
+    Creates a new struct dtype, with fields computed based
+    on the input fields. Leaves the requested number of
+    uniform dimensions in place, so the result has fewer
+    than the input if positive.
+    
+    Each field_expr should be a string or bit of code
+    that can be evaluated with an 'eval' call. It is called
+    with numpy/scipy in the globals, and the input
+    fields in the locals.
+    
+    Parameters
+    ----------
+    n : ndobject
+        This should have a uniform struct dtype. The
+        result will be a view of this data.
+    replace_undim : integer
+        The number of uniform dimensions to leave in the
+        input going to the fields. For example if the
+        input has shape (3,4,2) and replace_undim is 1,
+        the result will have shape (3,4), and each operand
+        provided to the field expression will have shape (2).
+    fields : list of (field_name, field_type, field_expr)
+        These are the fields which are created in the output.
+        No fields are retained from the input.
+    fnname : string, optional
+        The function name, which affects how the resulting
+        deferred expression's dtype is printed.
+
+    Examples
+    --------
+    >>> from dynd import nd, ndt
+    >>> a = nd.ndobject([
+    ...  ('A', 1, 2), ('A', 3, 4),
+    ...  ('B', 1.5, 2.5), ('A', 0.5, 9),
+    ...  ('C', 1, 5), ('B', 2, 2)],
+    ...  udtype='{cat: string; x: float32; y: float32}')
+    >>> gb = nd.groupby(a, a.cat)
+    >>> gb.groups
+    nd.ndobject(["A", "B", "C"], strided_dim<string>)
+    >>> b = nd.make_computed_fields(gb.eval(), 1,
+    ...                 fields=[('sum_x', ndt.float32, 'sum(x)'),
+    ...                         ('mean_y', ndt.float32, 'mean(y)'),
+    ...                         ('max_x', ndt.float32, 'max(x)'),
+    ...                         ('max_y', ndt.float32, 'max(y)'),
+    ...                         ('min_y', ndt.float32, 'min(y)')])
+    >>> from pprint import pprint
+    >>> pprint(nd.as_py(b))
+    [{u'max_x': 3.0, u'max_y': 9.0, u'mean_y': 5.0, u'min_y': 2.0, u'sum_x': 4.5},
+     {u'max_x': 2.0, u'max_y': 2.5, u'mean_y': 2.25, u'min_y': 2.0, u'sum_x': 3.5},
+     {u'max_x': 1.0, u'max_y': 5.0, u'mean_y': 5.0, u'min_y': 5.0, u'sum_x': 1.0}]
+    """
     n = ndobject(n)
     udt = n.udtype.value_dtype
     if udt.kind != 'struct':

d662605a75667228bb4c7c11dca2bf9f1605f96d
Mark Wiebe <mwwiebe@gmail.com>
2013-04-24 09:51:01 -0500

    Add error checking to jenkins .bat file

--- jenkins/jenkins-build.bat
+++ jenkins/jenkins-build.bat
@@ -34,6 +34,7 @@ REM Configure the appropriate visual studio command line environment
 if "%PROGRAMFILES(X86)%" == "" set VCDIR=%PROGRAMFILES%\Microsoft Visual Studio %MSVC_VERSION%\VC
 if NOT "%PROGRAMFILES(X86)%" == "" set VCDIR=%PROGRAMFILES(X86)%\Microsoft Visual Studio %MSVC_VERSION%\VC
 call "%VCDIR%\vcvarsall.bat" %MSVC_VCVARS_PLATFORM%
+IF %ERRORLEVEL% NEQ 0 exit /b 1
 echo on
 
 REM Remove the build subdirectory from last time
@@ -43,22 +44,28 @@ REM Get libdynd into the libraries subdirectory
 REM TODO: Build libdynd in a separate jenkins project,
 REM       and use its build artifact here.
 call .\jenkins\checkout_libdynd.bat
+IF %ERRORLEVEL% NEQ 0 exit /b 1
 
 REM Use conda to create a conda environment of the required
 REM python version and containing the dependencies.
 SET PYENV_PREFIX=%WORKSPACE%\build\pyenv
 call .\jenkins\create_conda_pyenv.bat 2.7 %PYENV_PREFIX%
+IF %ERRORLEVEL% NEQ 0 exit /b 1
 echo on
 export PATH=%PYENV_PREFIX%;%PYENV_PREFIX%\Scripts;%PATH%
 
 REM Create a fresh visual studio solution with cmake, and do the build/install
 cd build
 cmake -DCMAKE_INSTALL_PREFIX=install -G %CMAKE_BUILD_TARGET% ..
+IF %ERRORLEVEL% NEQ 0 exit /b 1
 devenv dynd-python.sln /Build "RelWithDebInfo|%MSVC_BUILD_PLATFORM%"
+IF %ERRORLEVEL% NEQ 0 exit /b 1
 devenv dynd-python.sln /Build "RelWithDebInfo|%MSVC_BUILD_PLATFORM%" /Project INSTALL
+IF %ERRORLEVEL% NEQ 0 exit /b 1
 
 REM Run the tests and generate xml results
 python -c "import dynd;dynd.test(xunitfile='../test_results.xml', exit=1)"
+IF %ERRORLEVEL% NEQ 0 exit /b 1
 
 REM Get the version number and process it into a suitable form
 FOR /F "delims=" %%i IN ('python -c "import dynd;print(dynd.__version_string__)"') DO set PYDYND_VERSION=%%i
@@ -73,6 +80,9 @@ cd pkgs
 
 # Create a conda package from the build
 call conda package -p %PYENV_PREFIX% --pkg-name=dynd --pkg-version=%PYDYND_VERSION%
+IF %ERRORLEVEL% NEQ 0 exit /b 1
 echo on
 
 cd ..
+
+exit /b 0

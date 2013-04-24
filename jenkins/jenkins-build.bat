974ddb52d1faa8d466510e22fd802c90dac633f4
Mark Wiebe <mwwiebe@gmail.com>
2013-04-24 10:09:38 -0500

    Try specifying the python interpreter to cmake directly

--- jenkins/jenkins-build.bat
+++ jenkins/jenkins-build.bat
@@ -14,20 +14,29 @@ REM   - Call the script from the root workspace
 REM     directory as ./jenkins/jenkins-build
 REM
 
+REM If no MSVC version is selected, choose 2010
+if "%MSVC_VERSION%" == "" set MSVC_VERSION=10.0
+REM If no PYTHON version is selected, choose 2.7
+if "%PYTHON_VERSION%" == "" set PYTHON_VERSION=2.7
+
 REM Jenkins has '/' in its workspace. Fix it to '\' to simplify the DOS commands.
 set WORKSPACE=%WORKSPACE:/=\%
 
 if "%PROCESSOR_ARCHITECTURE%" == "AMD64" goto :amd64
- set MSVC_VERSION=10.0
  set MSVC_VCVARS_PLATFORM=x86
  set MSVC_BUILD_PLATFORM=Win32
- set CMAKE_BUILD_TARGET="Visual Studio 10"
+ if "%MSVC_VERSION%" == "9.0" set CMAKE_BUILD_TARGET="Visual Studio 9 2008"
+ if "%MSVC_VERSION%" == "10.0" set CMAKE_BUILD_TARGET="Visual Studio 10"
+ if "%MSVC_VERSION%" == "11.0" set CMAKE_BUILD_TARGET="Visual Studio 11"
 goto :notamd64
 :amd64
  set MSVC_VERSION=10.0
  set MSVC_VCVARS_PLATFORM=amd64
  set MSVC_BUILD_PLATFORM=x64
  set CMAKE_BUILD_TARGET="Visual Studio 10 Win64"
+ if "%MSVC_VERSION%" == "9.0" set CMAKE_BUILD_TARGET="Visual Studio 9 2008 Win64"
+ if "%MSVC_VERSION%" == "10.0" set CMAKE_BUILD_TARGET="Visual Studio 10 Win64"
+ if "%MSVC_VERSION%" == "11.0" set CMAKE_BUILD_TARGET="Visual Studio 11 Win64"
 :notamd64
 
 REM Configure the appropriate visual studio command line environment
@@ -49,14 +58,14 @@ IF %ERRORLEVEL% NEQ 0 exit /b 1
 REM Use conda to create a conda environment of the required
 REM python version and containing the dependencies.
 SET PYENV_PREFIX=%WORKSPACE%\build\pyenv
-call .\jenkins\create_conda_pyenv.bat 2.7 %PYENV_PREFIX%
+call .\jenkins\create_conda_pyenv.bat %PYTHON_VERSION% %PYENV_PREFIX%
 IF %ERRORLEVEL% NEQ 0 exit /b 1
 echo on
-export PATH=%PYENV_PREFIX%;%PYENV_PREFIX%\Scripts;%PATH%
+REM export PATH=%PYENV_PREFIX%;%PYENV_PREFIX%\Scripts;%PATH%
 
 REM Create a fresh visual studio solution with cmake, and do the build/install
 cd build
-cmake -DCMAKE_INSTALL_PREFIX=install -G %CMAKE_BUILD_TARGET% ..
+cmake -DCMAKE_INSTALL_PREFIX=install -G %CMAKE_BUILD_TARGET% -DPYTHON_EXECUTABLE=%PYENV_PREFIX%\Python.exe ..
 IF %ERRORLEVEL% NEQ 0 exit /b 1
 devenv dynd-python.sln /Build "RelWithDebInfo|%MSVC_BUILD_PLATFORM%"
 IF %ERRORLEVEL% NEQ 0 exit /b 1

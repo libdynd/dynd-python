6931ed57e49e8f2d62257cba2498384f4e47956f
Mark Wiebe <mwwiebe@gmail.com>
2013-06-19 15:25:50 -0700

    Add nose to the environment creation

--- buildscripts/create_conda_pyenv.bat
+++ buildscripts/create_conda_pyenv.bat
@@ -2,5 +2,5 @@ REM $1 is the python version
 REM $2 is the directory in which the conda env is created
 
 rd /q /s %2
-call C:\Anaconda\Scripts\conda create --yes -p %2 python=%1 cython=0.19 scipy
+call C:\Anaconda\Scripts\conda create --yes -p %2 python=%1 cython=0.19 scipy nose
 echo on

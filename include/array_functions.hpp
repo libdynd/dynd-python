276773d7f76ea853389bd9dd361b7731788c9887
talumbau <tj.alumbaugh@continuum.io>
2014-04-14 16:49:18 -0500

    fixed typo

--- include/array_functions.hpp
+++ include/array_functions.hpp
@@ -86,7 +86,7 @@ dynd::nd::array array_ones(PyObject *shape, const dynd::ndt::type& d, PyObject *
 dynd::nd::array array_full(const dynd::ndt::type& d, PyObject *value, PyObject *access);
 dynd::nd::array array_full(PyObject *shape, const dynd::ndt::type& d, PyObject *value, PyObject *access);
 
-dynd::nd::array array_empty(const dynd::ndt::type& d, PyObject *acess);
+dynd::nd::array array_empty(const dynd::ndt::type& d, PyObject *access);
 dynd::nd::array array_empty(PyObject *shape, const dynd::ndt::type& d, PyObject *access);
 
 inline dynd::nd::array array_empty_like(const dynd::nd::array& n)

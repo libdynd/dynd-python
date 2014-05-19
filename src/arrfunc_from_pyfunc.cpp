8d949060ccd8859166d4ebe5f41796a58326f205
Mark Wiebe <mwiebe@continuum.io>
2014-05-19 00:44:52 -0700

    build fix

--- src/arrfunc_from_pyfunc.cpp
+++ src/arrfunc_from_pyfunc.cpp
@@ -52,10 +52,10 @@ namespace {
         {
             switch (kernreq) {
             case kernel_request_single:
-                base.template set_function<expr_single_operation_t>(&self_type::single);
+                base.set_function<expr_single_operation_t>(&self_type::single);
                 break;
             case kernel_request_strided:
-                base.template set_function<expr_strided_operation_t>(&self_type::strided);
+                base.set_function<expr_strided_operation_t>(&self_type::strided);
                 break;
             default: {
                 std::stringstream ss;
@@ -178,11 +178,10 @@ namespace {
         const arrfunc_type_data *af_self, dynd::ckernel_builder *ckb, intptr_t ckb_offset,
         const ndt::type &dst_tp, const char *dst_arrmeta,
         const ndt::type *src_tp, const char *const *src_arrmeta,
-        uint32_t kernreq, const eval::eval_context *ectx)
+        uint32_t kernreq, const eval::eval_context *DYND_UNUSED(ectx))
     {
         typedef pyfunc_expr_ck self_type;
         PyGILState_RAII pgs;
-        PyObject *pyfunc = reinterpret_cast<PyObject *>(af_self->data_ptr);
         intptr_t param_count = af_self->get_param_count();
 
         self_type *self = self_type::create(ckb, ckb_offset, (kernel_request_t)kernreq);

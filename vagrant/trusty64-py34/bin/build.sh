da7adf6dcf8af1df0722e61ba2768da3a95e592d
Phillip Cloud <cpcloud@gmail.com>
2014-08-02 14:20:15 -0400

    CLN: cmake should not use the curdir

--- vagrant/trusty64-py34/bin/build.sh
+++ vagrant/trusty64-py34/bin/build.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 pushd ~/libdynd/build || exit 1
-cmake .
+cmake ..
 make
 make install
 sudo ldconfig
 popd
 
 pushd ~/dynd-python/build || exit 1
-cmake .
+cmake ..
 make
 make install
 popd

71501c9d76a4ff3458f7f448fbbb72f228e99c20
Mark Wiebe <mwwiebe@gmail.com>
2015-02-25 23:52:38 -0800

    Fix the pattern match tests

--- dynd/tests/test_type_pattern_match.py
+++ dynd/tests/test_type_pattern_match.py
@@ -4,20 +4,20 @@ from dynd import nd, ndt
 
 class TestTypePatternMatch(unittest.TestCase):
     def test_simple(self):
-        self.assertTrue(ndt.int32.matches(ndt.int32))
-        self.assertTrue(ndt.type('T').matches(ndt.int16))
-        self.assertTrue(ndt.type('... * T').matches(ndt.int16))
-        self.assertTrue(ndt.type('A... * T').matches(ndt.int16))
-        self.assertTrue(ndt.type('M * A... * N * T').matches(ndt.type('Fixed * var * int')))
-        self.assertFalse(ndt.type('M * A... * N * T').matches(ndt.type('Fixed * int')))
+        self.assertTrue(ndt.int32.match(ndt.int32))
+        self.assertTrue(ndt.type('T').match(ndt.int16))
+        self.assertTrue(ndt.type('... * T').match(ndt.int16))
+        self.assertTrue(ndt.type('A... * T').match(ndt.int16))
+        self.assertTrue(ndt.type('M * A... * N * T').match(ndt.type('Fixed * var * int')))
+        self.assertFalse(ndt.type('M * A... * N * T').match(ndt.type('Fixed * int')))
 
     def test_tuple(self):
         pat = ndt.type('(T, ?T, 3 * T, A... * S)')
-        self.assertTrue(pat.matches(ndt.type('(int, ?int, 3 * int, real)')))
-        self.assertTrue(pat.matches(ndt.type('(string, ?string, 3 * string, 10 * complex)')))
-        self.assertFalse(pat.matches(ndt.type('(string, ?int, 3 * string, 10 * complex)')))
-        self.assertFalse(pat.matches(ndt.type('(string, string, 3 * string, 10 * complex)')))
-        self.assertFalse(pat.matches(ndt.type('(string, ?string, 4 * string, 10 * complex)')))
+        self.assertTrue(pat.match(ndt.type('(int, ?int, 3 * int, real)')))
+        self.assertTrue(pat.match(ndt.type('(string, ?string, 3 * string, 10 * complex)')))
+        self.assertFalse(pat.match(ndt.type('(string, ?int, 3 * string, 10 * complex)')))
+        self.assertFalse(pat.match(ndt.type('(string, string, 3 * string, 10 * complex)')))
+        self.assertFalse(pat.match(ndt.type('(string, ?string, 4 * string, 10 * complex)')))
 
 if __name__ == '__main__':
     unittest.main()

e176a1954a0af2483ce0770f4181d8de397c5296
izaid <hi@irwinzaid.com>
2015-07-29 01:33:16 +0100

    Added basic Numba example

--- dynd/nd/functional.pxd
+++ dynd/nd/functional.pxd
@@ -1,3 +1,5 @@
+from libc.stdint cimport intptr_t
+
 from ..config cimport translate_exception
 from dynd.ndt.type cimport _type
 from .callable cimport _callable
@@ -7,6 +9,9 @@ cdef extern from 'dynd/func/elwise.hpp' namespace 'dynd::nd::functional':
 
 cdef extern from "arrfunc_from_pyfunc.hpp" namespace "pydynd::nd::functional":
     _callable _apply 'pydynd::nd::functional::apply'(object, object) except +translate_exception
+
+cdef extern from "kernels/apply_numba_kernel.hpp" namespace "pydynd::nd::functional":
+    _callable numba_helper "pydynd::nd::functional::numba_helper"(const _type &tp, intptr_t)
 
 cdef extern from 'dynd/func/multidispatch.hpp' namespace 'dynd::nd::functional':
     _callable _multidispatch 'dynd::nd::functional::multidispatch'[T](_type, T, T) except +translate_exception
--- dynd/nd/functional.pyx
+++ dynd/nd/functional.pyx
@@ -25,3 +25,70 @@ def elwise(func):
 def multidispatch(type tp, iterable = None):
     return wrap(_multidispatch(tp.v, begin[_callable](iterable),
         end[_callable](iterable)))
+
+def apply_ptr(tp, ptr):
+    return wrap(numba_helper((<type> tp).v, <intptr_t> ptr))
+
+import numba
+
+dynd_to_numba = {}
+dynd_to_numba[ndt.type('int32').type_id] = numba.int32
+dynd_to_numba[ndt.type('int64').type_id] = numba.int64
+
+def apply_numba(tp, f):
+    import llvmlite.ir as ll
+    import llvmlite.binding as llvm
+
+    from numba import types, compiler, njit, cgutils
+
+    print tp.pos_types
+
+    res = f.compile(tuple(types.int32 for i in range(2)))
+    sig = f.signatures[0]
+    cres = f._compileinfos[sig]
+
+    fndesc = cres.fndesc
+    library = cres.library
+    target = cres.target_context
+
+    codegen = target.jit_codegen()
+    wrapper_library = codegen.create_library('dynd_example_library')
+    mod = wrapper_library.create_ir_module(name='dynd_example')
+
+    inner_func_type = target.call_conv.get_function_type(fndesc.restype,
+                                                     fndesc.argtypes)
+
+    inner_func = mod.get_or_insert_function(inner_func_type,
+                                            name=fndesc.llvm_func_name)
+
+    CharType = ll.IntType(8)
+
+    single = ll.Function(mod, ll.FunctionType(ll.VoidType(),
+        [CharType.as_pointer(), CharType.as_pointer().as_pointer()]),
+        name = 'single')
+
+    bb_entry = single.append_basic_block('entry')
+    irbuilder = ll.IRBuilder(bb_entry)
+
+    dst_tp = ll.IntType(64)
+    src_tp = [ll.IntType(32), ll.IntType(32)]
+
+    src = []
+    for i in range(2):
+        src.append(irbuilder.load(irbuilder.bitcast(irbuilder.load(irbuilder.gep(single.args[1],
+            [ll.Constant(ll.IntType(64), i)])), src_tp[i].as_pointer())))
+
+
+    status, dst = target.call_conv.call_function(irbuilder, inner_func,
+                                                    fndesc.restype,
+                                                    fndesc.argtypes,
+                                                    src)
+    
+    irbuilder.store(dst,
+        irbuilder.bitcast(single.args[0], dst_tp.as_pointer()))
+    irbuilder.ret_void()
+
+    wrapper_library.add_ir_module(mod)
+    wrapper_library.finalize()
+
+    return apply_ptr(tp, wrapper_library.get_pointer_to_function('single'))

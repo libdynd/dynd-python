2ee6ab6142bd755239c99d81115f494fa475a041
izaid <hi@irwinzaid.com>
2015-08-06 09:46:10 +0100

    Added conda.recipe

---
+++ conda.recipe/build.sh
@@ -0,0 +1,18 @@
+et -ex
+
+cd $RECIPE_DIR
+
+echo Setting the compiler...
+if [ `uname` == Linux ]; then
+    export CC="$PREFIX/bin/gcc"
+    export CXX="$PREFIX/bin/g++"
+elif [ `uname` == Darwin ]; then
+    CPPFLAGS="-stdlib=libc++"
+    EXTRAOPTIONS="-DCMAKE_OSX_DEPLOYMENT_TARGET=10.8"
+    MACOSX_DEPLOYMENT_TARGET=10.8
+else
+    CPPFLAGS=
+    EXTRAOPTIONS=
+fi
+
+python setup.py install || exit 1

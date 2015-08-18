ac8c397fc854856e218531ff64c98adcc160610e
izaid <hi@irwinzaid.com>
2015-08-18 14:13:41 +0100

    Disable building libdynd tests on Travis

--- setup.py
+++ setup.py
@@ -80,7 +80,7 @@ class cmake_build_ext(build_ext):
             cuda_option = '-DDYND_CUDA=ON'
 
     if sys.platform != 'win32':
-        self.spawn(['cmake', pyexe_option, install_lib_option,
+        self.spawn(['cmake', pyexe_option, install_lib_option, build_tests_option,
                     static_lib_option, cuda_option, source])
         self.spawn(['make'])
     else:

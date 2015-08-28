60c41ff91adad4ae8e96b961870f114c54f3cb7d
Irwin Zaid <izaid@users.noreply.github.com>
2015-08-28 11:50:15 +0100

    Update build.sh

--- conda.recipe/build.sh
+++ conda.recipe/build.sh
@@ -9,8 +9,8 @@ if [ `uname` == Linux ]; then
     export CXX="$PREFIX/bin/g++"
 elif [ `uname` == Darwin ]; then
 #    CPPFLAGS="-stdlib=libc++"
-    export CC="$PREFIX/bin/gcc"
-    export CXX="$PREFIX/bin/g++"
+#    export CC="$PREFIX/bin/gcc"
+#    export CXX="$PREFIX/bin/g++"
     EXTRAOPTIONS="-DCMAKE_OSX_DEPLOYMENT_TARGET=10.9"
     MACOSX_DEPLOYMENT_TARGET=10.9
 else

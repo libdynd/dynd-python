a4fb70f1bab0147c63ffe0a303a100ba31a8b658
Irwin Zaid <izaid@users.noreply.github.com>
2015-08-30 12:20:23 +0100

    Update build.sh

--- conda.recipe/build.sh
+++ conda.recipe/build.sh
@@ -4,19 +4,19 @@ set -ex
 cd $RECIPE_DIR
 
 echo Setting the compiler...
-if [ `uname` == Linux ]; then
-    export CC="$PREFIX/bin/gcc"
-    export CXX="$PREFIX/bin/g++"
-elif [ `uname` == Darwin ]; then
+#if [ `uname` == Linux ]; then
+ #   export CC="$PREFIX/bin/gcc"
+  #  export CXX="$PREFIX/bin/g++"
+#elif [ `uname` == Darwin ]; then
 #    CPPFLAGS="-stdlib=libc++"
 #    export CC="$PREFIX/bin/gcc"
 #    export CXX="$PREFIX/bin/g++"
-    EXTRAOPTIONS="-DCMAKE_OSX_DEPLOYMENT_TARGET=10.9"
-    MACOSX_DEPLOYMENT_TARGET=10.9
-else
-    CPPFLAGS=
-    EXTRAOPTIONS=
-fi
+ #   EXTRAOPTIONS="-DCMAKE_OSX_DEPLOYMENT_TARGET=10.9"
+  #  MACOSX_DEPLOYMENT_TARGET=10.9
+#else
+ #   CPPFLAGS=
+  #  EXTRAOPTIONS=
+#fi
 
 cd ..
 $PYTHON setup.py install || exit 1

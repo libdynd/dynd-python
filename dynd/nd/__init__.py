8c624267c026fa57be6cdd0004448af8f6e3213c
izaid <hi@irwinzaid.com>
2015-10-25 10:37:29 +0000

    Removed groupby wrapper

--- dynd/nd/__init__.py
+++ dynd/nd/__init__.py
@@ -4,7 +4,7 @@ from ..eval_context import eval_context, modify_default_eval_context
 from .array import array, asarray, type_of, dshape_of, as_py, view, \
     ones, zeros, empty, full, is_c_contiguous, is_f_contiguous, range, \
     parse_json, squeeze, dtype_of, linspace, fields, ndim_of, as_numpy, \
-    groupby, parse as _parse
+    parse as _parse
 from .callable import callable
 
 inf = float('inf')

d4f082048edcb7cdbe431f9c6366c1df7bb872b8
izaid <hi@irwinzaid.com>
2015-10-29 17:14:22 +0000

    Different fix for Python 3

--- dynd/tests/test_unicode.py
+++ dynd/tests/test_unicode.py
@@ -14,9 +14,10 @@ class TestUnicode(unittest.TestCase):
 
     def test_bytes_string(self):
         if sys.version_info >= (3, 0):
-            pass # This needs to be fixed for Python 3
-#            a = nd.array(b"Testing 1 2 3")
-#            b = nd.array([b"First", b"Second"])
+            return
+            # This needs to be fixed for Python 3
+            a = nd.array(b"Testing 1 2 3")
+            b = nd.array([b"First", b"Second"])
         else:
             # In Python 2, str and bytes are the same,
             # so we have to manually request a bytes type

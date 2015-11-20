a8009afd7eef430fd3fee8df6a6b651fcd38dd09
Ian Henriksen <insertinterestingnamehere@gmail.com>
2015-11-20 12:13:53 -0700

    Remove build directory when building conda package.

--- conda.recipe/bld.bat
+++ conda.recipe/bld.bat
@@ -1,6 +1,9 @@
 cd %RECIPE_DIR%
 cd ..
 
+:: Remove the build directory since incremental builds aren't currently supported.
+rd /s /q build
+
 python setup.py install || exit 1
 rd /s /q %SP_DIR%\__pycache__
 rd /s /q %SP_DIR%\numpy

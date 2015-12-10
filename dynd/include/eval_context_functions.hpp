6a45289bcb35556fe313bdf66892570a321c138f
Ian Henriksen <insertinterestingnamehere@gmail.com>
2015-12-10 14:07:49 -0700

    Remove unused eval_context routines.

--- dynd/include/eval_context_functions.hpp
+++ dynd/include/eval_context_functions.hpp
@@ -23,10 +23,7 @@ namespace pydynd {
  * This is the typeobject and struct of w_eval_context from Cython.
  */
 PYDYND_API extern PyTypeObject *WEvalContext_Type;
-inline bool WEvalContext_CheckExact(PyObject *obj)
-{
-  return Py_TYPE(obj) == WEvalContext_Type;
-}
+
 inline bool WEvalContext_Check(PyObject *obj)
 {
   return PyObject_TypeCheck(obj, WEvalContext_Type);
@@ -37,22 +34,6 @@ struct WEvalContext {
   bool own_ectx;
 };
 PYDYND_API void init_w_eval_context_typeobject(PyObject *type);
-
-/**
- * Makes a copy of an eval context, owned by a WEvalContext.
- */
-inline PyObject *wrap_eval_context(const dynd::eval::eval_context *ectx)
-{
-  WEvalContext *result =
-      (WEvalContext *)WEvalContext_Type->tp_alloc(WEvalContext_Type, 0);
-  if (!result) {
-    throw std::runtime_error("");
-  }
-  result->own_ectx = false;
-  result->ectx = new dynd::eval::eval_context(*ectx);
-  result->own_ectx = true;
-  return (PyObject *)result;
-}
 
 inline const dynd::eval::eval_context *eval_context_from_pyobj(PyObject *obj)
 {

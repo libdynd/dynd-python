42a6b42bbfa25e1d7fc16d5ef0cbc234a6c8effe
Irwin Zaid <hi@irwinzaid.com>
2016-01-04 11:43:52 -0600

    Migrated a bunch of copy_from_pyobject kernels into assignment

--- dynd/nd/array.pyx
+++ dynd/nd/array.pyx
@@ -161,7 +161,11 @@ cdef class array(object):
         from . import assign
 
         cdef _array a
-        ids = [bool_type_id, int8_type_id, int16_type_id, int32_type_id, int64_type_id, uint8_type_id, uint16_type_id, uint32_type_id, uint64_type_id, option_type_id]
+        ids = [bool_type_id, int8_type_id, int16_type_id, int32_type_id, int64_type_id, int128_type_id,
+            uint8_type_id, uint16_type_id, uint32_type_id, uint64_type_id, uint128_type_id,
+            float16_type_id, float32_type_id, float64_type_id, complex_float32_type_id, complex_float64_type_id,
+            bytes_type_id, fixed_bytes_type_id, string_type_id, date_type_id, time_type_id, datetime_type_id,
+            option_type_id, type_type_id]
         if value is not Unsupplied:
             # Get the array data
             if type is not None:
--- dynd/src/copy_from_pyobject_arrfunc.cpp
+++ dynd/src/copy_from_pyobject_arrfunc.cpp
@@ -69,19 +69,19 @@ PYDYND_API void init_assign(nd::callable &assign)
   typedef type_id_sequence<
       bool_type_id, int8_type_id, int16_type_id, int32_type_id, int64_type_id,
       int128_type_id, uint8_type_id, uint16_type_id, uint32_type_id,
-      uint64_type_id, uint128_type_id, option_type_id> type_ids;
+      uint64_type_id, uint128_type_id, float16_type_id, float32_type_id,
+      float64_type_id, complex_float32_type_id, complex_float64_type_id,
+      bytes_type_id, fixed_bytes_type_id, string_type_id, fixed_string_type_id,
+      date_type_id, time_type_id, datetime_type_id, option_type_id,
+      type_type_id> type_ids;
 
   for (const auto &pair : nd::callable::make_all<assign_kernel, type_ids>()) {
     assign.set_overload(pair.first, {ndt::make_type<pyobject_type>()},
                         pair.second);
   }
 
-  typedef type_id_sequence<float16_type_id, float32_type_id, float64_type_id,
-                           complex_float32_type_id, complex_float64_type_id,
-                           bytes_type_id, fixed_bytes_type_id, string_type_id,
-                           fixed_string_type_id, date_type_id, time_type_id,
-                           datetime_type_id, type_type_id, categorical_type_id,
-                           fixed_dim_type_id, var_dim_type_id, tuple_type_id,
+  typedef type_id_sequence<categorical_type_id, fixed_dim_type_id,
+                           var_dim_type_id, tuple_type_id,
                            struct_type_id> old_type_ids;
 
   for (const auto &pair :

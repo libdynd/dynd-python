e8337a53a4738a8f5df3cda29fb069b322d6e495
Ian Henriksen <insertinterestingnamehere@gmail.com>
2016-01-27 10:23:40 -0700

    Add postprocessing step to build for Cython that allows for patching the generated files to work around any Cython bugs that aren't fixed in the latest Cython release.

---
+++ postprocess.py
@@ -0,0 +1,14 @@
+from __future__ import print_function
+
+def postprocess(files):
+    for cpp_file in files:
+        print('Postprocessing {}'.format(cpp_file))
+        with open(cpp_file, 'r') as f:
+            lines = f.readlines()
+        lines = [line for line in lines if not line.startswith('#line')]
+        with open(cpp_file, 'w') as f:
+            f.writelines(lines)
+
+if __name__ == '__main__':
+    from sys import argv
+    postprocess(argv[1:])

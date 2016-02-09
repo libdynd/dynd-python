37b66555a4c9faa6c4acb156b63f1a8a1dc4b9b1
Irwin Zaid <hi@irwinzaid.com>
2016-02-08 21:17:19 -0600

    Removed some unneeded code in registry.pyx

--- dynd/nd/registry.pyx
+++ dynd/nd/registry.pyx
@@ -1,10 +1,5 @@
 # cython: c_string_type=str, c_string_encoding=ascii
 
-from cython.operator cimport dereference, postincrement
-
-from libcpp.map cimport map
-from libcpp.string cimport string
-
 from ..cpp.func.callable cimport callable
 from ..cpp.func.callable_registry cimport callable_registry
 
@@ -16,12 +11,6 @@ cdef extern from 'assign.hpp':
 
 assign_init()
 
-cdef extern from *:
-    bint is_py_2 "(PY_MAJOR_VERSION == 2)"
-
 def get_published_callables():
-    py_reg = dict()
     for pair in callable_registry:
-        py_reg[pair.first] = dynd_nd_callable_from_cpp(pair.second)
-
-    return py_reg
+        yield pair.first, dynd_nd_callable_from_cpp(pair.second)

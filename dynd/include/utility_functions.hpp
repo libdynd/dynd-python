d19c7a2c764cd7cc0f030061946affd2d8072572
Ian Henriksen <insertinterestingnamehere@gmail.com>
2016-02-10 21:25:46 -0700

    Use clang-format on utility function header.

--- dynd/include/utility_functions.hpp
+++ dynd/include/utility_functions.hpp
@@ -118,23 +118,23 @@ public:
  * Function which casts the parameter to
  * a PyObject pointer and calls Py_XDECREF on it.
  */
- inline void py_decref_function(void *obj)
- {
-   // Because dynd in general is intended to do things multi-threaded
-   // (eventually),
-   // the decref function needs to be threadsafe. The way to do that is to ensure
-   // we're holding the GIL. This is slower than normal DECREF, but because the
-   // reference count isn't an atomic variable, this appears to be the best we
-   // can do.
-   if (obj != NULL) {
-     PyGILState_STATE gstate;
-     gstate = PyGILState_Ensure();
+inline void py_decref_function(void *obj)
+{
+  // Because dynd in general is intended to do things multi-threaded
+  // (eventually),
+  // the decref function needs to be threadsafe. The way to do that is to ensure
+  // we're holding the GIL. This is slower than normal DECREF, but because the
+  // reference count isn't an atomic variable, this appears to be the best we
+  // can do.
+  if (obj != NULL) {
+    PyGILState_STATE gstate;
+    gstate = PyGILState_Ensure();
 
-     Py_DECREF((PyObject *)obj);
+    Py_DECREF((PyObject *)obj);
 
-     PyGILState_Release(gstate);
-   }
- }
+    PyGILState_Release(gstate);
+  }
+}
 
 inline intptr_t pyobject_as_index(PyObject *index)
 {
@@ -143,10 +143,12 @@ inline intptr_t pyobject_as_index(PyObject *index)
   if (PyLong_Check(start_obj.get())) {
     result = PyLong_AsSsize_t(start_obj.get());
 #if PY_VERSION_HEX < 0x03000000
-  } else if (PyInt_Check(start_obj.get())) {
+  }
+  else if (PyInt_Check(start_obj.get())) {
     result = PyInt_AS_LONG(start_obj.get());
 #endif
-  } else {
+  }
+  else {
     throw std::runtime_error(
         "Value returned from PyNumber_Index is not an int or long");
   }
@@ -168,7 +170,8 @@ inline int pyobject_as_int_index(PyObject *index)
     throw std::exception();
   }
   if (((unsigned long)result & 0xffffffffu) != (unsigned long)result) {
-    throw std::overflow_error("overflow converting Python integer to 32-bit int");
+    throw std::overflow_error(
+        "overflow converting Python integer to 32-bit int");
   }
   return (int)result;
 }
@@ -188,7 +191,8 @@ inline dynd::irange pyobject_as_irange(PyObject *index)
       result.set_step(pyobject_as_index(slice->step));
     }
     return result;
-  } else {
+  }
+  else {
     return dynd::irange(pyobject_as_index(index));
   }
 }
@@ -209,13 +213,15 @@ inline std::string pystring_as_string(PyObject *str)
     }
     return std::string(data, len);
 #if PY_VERSION_HEX < 0x03000000
-  } else if (PyString_Check(str)) {
+  }
+  else if (PyString_Check(str)) {
     if (PyString_AsStringAndSize(str, &data, &len) < 0) {
       throw std::runtime_error("Error getting string data");
     }
     return std::string(data, len);
 #endif
-  } else {
+  }
+  else {
     throw dynd::type_error("Cannot convert pyobject to string");
   }
 }
@@ -245,8 +251,9 @@ inline std::string pyobject_repr(PyObject *obj)
 // Forward declare this. Include type functions header at the bottom.
 inline dynd::ndt::type make__type_from_pyobject(PyObject *obj);
 
-inline void pyobject_as_vector__type(PyObject *list_of_types,
-                                 std::vector<dynd::ndt::type> &vector_of__types)
+inline void
+pyobject_as_vector__type(PyObject *list_of_types,
+                         std::vector<dynd::ndt::type> &vector_of__types)
 {
   Py_ssize_t size = PySequence_Size(list_of_types);
   vector_of__types.resize(size);
@@ -257,19 +264,19 @@ inline void pyobject_as_vector__type(PyObject *list_of_types,
 }
 
 inline void pyobject_as_vector_string(PyObject *list_string,
-                                     std::vector<std::string> &vector_string)
+                                      std::vector<std::string> &vector_string)
 {
-Py_ssize_t size = PySequence_Size(list_string);
-vector_string.resize(size);
-for (Py_ssize_t i = 0; i < size; ++i) {
-  pyobject_ownref item(PySequence_GetItem(list_string, i));
-  vector_string[i] = pystring_as_string(item.get());
-}
+  Py_ssize_t size = PySequence_Size(list_string);
+  vector_string.resize(size);
+  for (Py_ssize_t i = 0; i < size; ++i) {
+    pyobject_ownref item(PySequence_GetItem(list_string, i));
+    vector_string[i] = pystring_as_string(item.get());
+  }
 }
 
 inline void pyobject_as_vector_intp(PyObject *list_index,
-                                     std::vector<intptr_t> &vector_intp,
-                                     bool allow_int)
+                                    std::vector<intptr_t> &vector_intp,
+                                    bool allow_int)
 {
   if (allow_int) {
     // If permitted, convert an int into a size-1 list
@@ -300,10 +307,12 @@ inline void pyobject_as_vector_intp(PyObject *list_index,
         vector_intp.resize(1);
         vector_intp[0] = v;
         return;
-      } else if (PyErr_ExceptionMatches(PyExc_TypeError)) {
+      }
+      else if (PyErr_ExceptionMatches(PyExc_TypeError)) {
         // Swallow a type error, fall through to the sequence code
         PyErr_Clear();
-      } else {
+      }
+      else {
         // Propagate the error
         throw std::exception();
       }
@@ -373,13 +382,15 @@ inline void mark_axis(PyObject *int_axis, int ndim, dynd::bool1 *reduce_axes)
 
   if (value >= ndim || value < -ndim) {
     throw dynd::axis_out_of_bounds(value, ndim);
-  } else if (value < 0) {
+  }
+  else if (value < 0) {
     value += ndim;
   }
 
   if (!reduce_axes[value]) {
     reduce_axes[value] = true;
-  } else {
+  }
+  else {
     std::stringstream ss;
     ss << "axis " << value << " is specified more than once";
     throw std::runtime_error(ss.str());
@@ -393,37 +404,40 @@ inline void mark_axis(PyObject *int_axis, int ndim, dynd::bool1 *reduce_axes)
  *
  * Returns the number of axes which were set.
  */
- inline int pyarg_axis_argument(PyObject *axis, int ndim, dynd::bool1 *reduce_axes)
- {
-   int axis_count = 0;
+inline int pyarg_axis_argument(PyObject *axis, int ndim,
+                               dynd::bool1 *reduce_axes)
+{
+  int axis_count = 0;
 
-   if (axis == NULL || axis == Py_None) {
-     // None means use all the axes
-     for (int i = 0; i < ndim; ++i) {
-       reduce_axes[i] = true;
-     }
-     axis_count = ndim;
-   } else {
-     // Start with no axes marked
-     for (int i = 0; i < ndim; ++i) {
-       reduce_axes[i] = false;
-     }
-     if (PyTuple_Check(axis)) {
-       // A tuple of axes
-       Py_ssize_t size = PyTuple_GET_SIZE(axis);
-       for (Py_ssize_t i = 0; i < size; ++i) {
-         mark_axis(PyTuple_GET_ITEM(axis, i), ndim, reduce_axes);
-         axis_count++;
-       }
-     } else {
-       // Just one axis
-       mark_axis(axis, ndim, reduce_axes);
-       axis_count = 1;
-     }
-   }
+  if (axis == NULL || axis == Py_None) {
+    // None means use all the axes
+    for (int i = 0; i < ndim; ++i) {
+      reduce_axes[i] = true;
+    }
+    axis_count = ndim;
+  }
+  else {
+    // Start with no axes marked
+    for (int i = 0; i < ndim; ++i) {
+      reduce_axes[i] = false;
+    }
+    if (PyTuple_Check(axis)) {
+      // A tuple of axes
+      Py_ssize_t size = PyTuple_GET_SIZE(axis);
+      for (Py_ssize_t i = 0; i < size; ++i) {
+        mark_axis(PyTuple_GET_ITEM(axis, i), ndim, reduce_axes);
+        axis_count++;
+      }
+    }
+    else {
+      // Just one axis
+      mark_axis(axis, ndim, reduce_axes);
+      axis_count = 1;
+    }
+  }
 
-   return axis_count;
- }
+  return axis_count;
+}
 
 } // namespace pydynd
 

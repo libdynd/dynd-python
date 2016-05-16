85d6d11308d11a6832147963c805835a98b8539d
Stefan Krah <skrah@bytereef.org>
2016-05-16 10:38:33 +0200

    Link dynd.ndt against libdyndt only.

--- cmake/FindLibDyND.cmake
+++ cmake/FindLibDyND.cmake
@@ -59,6 +59,16 @@ if("${_LIBDYND_CONFIG}" STREQUAL "")
 
 else()
 
+    # Get the ndt library to link against.
+    execute_process(COMMAND "${_LIBDYND_CONFIG}" "-libndtname"
+                    RESULT_VARIABLE _DYND_SEARCH_SUCCESS
+                    OUTPUT_VARIABLE LIBNDT_NAME
+                    ERROR_VARIABLE _DYND_ERROR_VALUE
+                    OUTPUT_STRIP_TRAILING_WHITESPACE)
+    if(NOT _DYND_SEARCH_SUCCESS MATCHES 0)
+        message(FATAL_ERROR "Error getting ndt library name:\n${_DYND_ERROR_VALUE}")
+    endif()
+
     # Get the libraries to link against.
     execute_process(COMMAND "${_LIBDYND_CONFIG}" "-libnames"
                     RESULT_VARIABLE _DYND_SEARCH_SUCCESS
@@ -82,6 +92,9 @@ else()
     if(WIN32)
         string(REPLACE "\\" "/" LIBDYND_LIBRARY_DIR ${LIBDYND_LIBRARY_DIR})
     endif()
+
+    set(LIBNDT_LIBRARY "${LIBDYND_LIBRARY_DIR}/${LIBNDT_NAME}")
+
     set(LIBDYND_LIBRARIES "")
     foreach(_lib ${LIBDYND_LIBRARY_NAMES})
         LIST(APPEND LIBDYND_LIBRARIES "${LIBDYND_LIBRARY_DIR}/${_lib}")

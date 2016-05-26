e1a2bf6b824e826d2231ac15613084d2712c8981
izaid <hi@irwinzaid.com>
2016-05-26 19:08:18 +0100

    Updates to match libdynd

--- dynd/src/types/pyobject_type.cpp
+++ dynd/src/types/pyobject_type.cpp
@@ -13,21 +13,17 @@ using namespace dynd;
 
 const type_id_t pyobject_id = new_id("pyobject", any_kind_id);
 
-pyobject_type::pyobject_type()
-    : ndt::base_type(pyobject_id, sizeof(PyObject *), alignof(PyObject *),
-                     type_flag_none | type_flag_zeroinit, 0, 0, 0)
+pyobject_type::pyobject_type(type_id_t new_id)
+    : ndt::base_type(new_id, pyobject_id, sizeof(PyObject *), alignof(PyObject *), type_flag_none | type_flag_zeroinit,
+                     0, 0, 0)
 {
 }
 
 void pyobject_type::print_type(std::ostream &o) const { o << "pyobject"; }
 
-bool pyobject_type::operator==(const base_type &rhs) const
-{
-  return get_id() == rhs.get_id();
-}
+bool pyobject_type::operator==(const base_type &rhs) const { return get_id() == rhs.get_id(); }
 
-void pyobject_type::print_data(std::ostream &o, const char *arrmeta,
-                               const char *data) const
+void pyobject_type::print_data(std::ostream &o, const char *arrmeta, const char *data) const
 {
   PyObject *repr = PyObject_Repr(*reinterpret_cast<PyObject *const *>(data));
 #if PY_VERSION_HEX < 0x03000000

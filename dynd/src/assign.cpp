622b4b19502a474deac52daaeff96af662b6c8c4
izaid <hi@irwinzaid.com>
2016-05-27 08:56:55 +0100

    Removed references to detail::infos

--- dynd/src/assign.cpp
+++ dynd/src/assign.cpp
@@ -18,6 +18,65 @@
 using namespace std;
 using namespace dynd;
 
+// This is a temporary thing until the type inheritance changes are finished
+static ndt::type type_for_id(type_id_t id)
+{
+  switch (id) {
+  case bool_id:
+    return ndt::make_type<bool>();
+  case int8_id:
+    return ndt::make_type<int8_t>();
+  case int16_id:
+    return ndt::make_type<int16_t>();
+  case int32_id:
+    return ndt::make_type<int32_t>();
+  case int64_id:
+    return ndt::make_type<int64_t>();
+  case int128_id:
+    return ndt::make_type<int128>();
+  case uint8_id:
+    return ndt::make_type<uint8_t>();
+  case uint16_id:
+    return ndt::make_type<uint16_t>();
+  case uint32_id:
+    return ndt::make_type<uint32_t>();
+  case uint64_id:
+    return ndt::make_type<uint64_t>();
+  case uint128_id:
+    return ndt::make_type<uint128>();
+  case float32_id:
+    return ndt::make_type<float>();
+  case float64_id:
+    return ndt::make_type<double>();
+  case complex_float32_id:
+    return ndt::make_type<dynd::complex<float>>();
+  case complex_float64_id:
+    return ndt::make_type<dynd::complex<double>>();
+  case bytes_id:
+    return ndt::make_type<ndt::bytes_type>();
+  case fixed_bytes_id:
+    return ndt::make_type<ndt::fixed_bytes_kind_type>();
+  case string_id:
+    return ndt::make_type<ndt::string_type>();
+  case fixed_string_id:
+    return ndt::make_type<ndt::fixed_string_kind_type>();
+  case option_id:
+    return ndt::make_type<ndt::option_type>();
+  case type_id:
+    return ndt::make_type<ndt::type>();
+  case tuple_id:
+    return ndt::make_type<ndt::tuple_type>();
+  case struct_id:
+    return ndt::make_type<ndt::struct_type>();
+  case fixed_dim_id:
+    return ndt::make_type<ndt::fixed_dim_kind_type>();
+  case var_dim_id:
+    return ndt::make_type<ndt::var_dim_type>();
+  default:
+    throw std::runtime_error("unmappable type id " + std::to_string(id) + " in assign_init");
+  }
+}
+
 PYDYND_API void assign_init()
 {
   typedef type_sequence<bool, int8_t, int16_t, int32_t, int64_t, int128, uint8_t, uint16_t, uint32_t, uint64_t, uint128,
@@ -27,19 +86,12 @@ PYDYND_API void assign_init()
       types;
 
   PyDateTime_IMPORT;
-  // TODO: There should be an interface that allows adding overloads
-  // based on type id rather than on actual type. The current interface
-  // makes for a lot of mapping back and forth between types and their
-  // corresponding type ids.
-  // For now, just use the infos vector from the type registry to map
-  // ids back to their corresponding types.
-  auto infos = ndt::detail::infos();
 
   for (const auto &pair : nd::callable::make_all<pydynd::nd::assign_from_pyobject_callable, types>()) {
-    nd::assign.overload(infos[pair.first[0]].tp, {ndt::make_type<pyobject_type>()}, pair.second);
+    nd::assign.overload(type_for_id(pair.first[0]), {ndt::make_type<pyobject_type>()}, pair.second);
   }
   for (const auto &pair : nd::callable::make_all<pydynd::nd::assign_to_pyobject_callable, types>()) {
-    nd::assign.overload(ndt::make_type<pyobject_type>(), {infos[pair.first[0]].tp}, pair.second);
+    nd::assign.overload(ndt::make_type<pyobject_type>(), {type_for_id(pair.first[0])}, pair.second);
   }
 }
 
